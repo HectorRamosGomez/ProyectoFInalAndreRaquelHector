875770ff391d5c564a4aeefe7c7ca1b3bf0a321d
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-05-22 14:19:24 +0200

    Arreglos en las actividades

--- app/src/main/java/com/example/proyectofinaldam/login/MainActivity2Idiomas.kt
+++ app/src/main/java/com/example/proyectofinaldam/login/MainActivity2Idiomas.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectofinaldam.R
+import com.example.proyectofinaldam.login.pagina_principal.MainAplicacion
 
 class MainActivity2Idiomas: AppCompatActivity(){
 
